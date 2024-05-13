4e9dcd271a002a818ce3cbac54bb2e09711e3594
Karan Patel <s0377009salemstate@gmail.com>
2024-05-13 16:38:15 -0400

    fetch data from the backend database to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const URL = "http://localhost:4000/user/";
+
 function App() {
-	return <div className="title-font">App</div>;
+	const [users, setUser] = useState("");
+
+	const fetchData = async () => {
+		let result = "";
+
+		try {
+			const res = await axios.get(URL);
+			result = JSON.stringify(res);
+
+			setUser(result);
+		} catch (error) {
+			console.log("Error: ", error);
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return <>{users}</>;
 }
 
 export default App;
