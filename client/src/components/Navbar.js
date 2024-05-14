5ea6a6624a0b977a0a7487a31bc3031aa6393b32
Karan Patel <s0377009salemstate@gmail.com>
2024-05-14 15:28:50 -0400

    basic resposive navbar finished

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,7 +13,7 @@ import logoImg from "../images/logo.png";
 import profileImg from "../images/profile.png";
 
 const NavBar = () => {
-	const user = "asdfl";
+	const user = null;
 
 	return (
 		<Navbar variant="dark" expand="lg" className="navbar">
@@ -28,65 +28,72 @@ const NavBar = () => {
 						style={{ margin: 0 }}
 					/>
 				</Navbar.Brand>
-				<Navbar.Toggle
-					className="navbar-toogle-btn"
-					aria-controls="basic-navbar-nav"
-				/>
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="me-auto">
-						{/* <Nav.Link as={Link} to="/home">
-							Home
+				{user && (
+					<Nav className="me-right mx-3 mx-md-0">
+						<Nav.Link>
+							<Image
+								src={profileImg}
+								width="40"
+								height="40"
+								roundedCircle
+								alt="Profile"
+							/>
 						</Nav.Link>
-						<Nav.Link as={Link} to="/about">
-							About
-						</Nav.Link>
-						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-							<NavDropdown.Item as={Link} to="/action1">
-								Action 1
-							</NavDropdown.Item>
-							<NavDropdown.Item as={Link} to="/action2">
-								Action 2
-							</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item as={Link} to="/action3">
-								Action 3
-							</NavDropdown.Item> */}
-						{/* </NavDropdown> */}
 					</Nav>
-					<Nav className="me-right">
-						{!user && (
-							<Button
-								className="nav-btn login-btn mx-md-2 flex-row-reverse"
-								variant="outline-light"
-								as={Link}
-								to="/login"
-							>
-								Login
-							</Button>
-						)}
-						{!user && (
-							<Button
-								className="nav-btn signup-btn my-3 my-md-0 mt-1 mt-md-0 flex-row-reverse"
-								variant="outline-light"
-								as={Link}
-								to="/signup"
-							>
-								Sign Up
-							</Button>
-						)}
-						{user && (
-							<Nav.Link>
-								<Image
-									src={profileImg}
-									width="40"
-									height="40"
-									roundedCircle
-									alt="Profile"
-								/>
-							</Nav.Link>
-						)}
-					</Nav>
-				</Navbar.Collapse>
+				)}
+
+				{!user && (
+					<Navbar.Toggle
+						className="navbar-toogle-btn"
+						aria-controls="basic-navbar-nav"
+					/>
+				)}
+				{!user && (
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							{/* <Nav.Link as={Link} to="/home">
+									Home
+								</Nav.Link>
+								<Nav.Link as={Link} to="/about">
+									About
+								</Nav.Link>
+								<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+									<NavDropdown.Item as={Link} to="/action1">
+										Action 1
+									</NavDropdown.Item>
+									<NavDropdown.Item as={Link} to="/action2">
+										Action 2
+									</NavDropdown.Item>
+									<NavDropdown.Divider />
+									<NavDropdown.Item as={Link} to="/action3">
+										Action 3
+									</NavDropdown.Item> */}
+							{/* </NavDropdown> */}
+						</Nav>
+						<Nav className="me-right">
+							{!user && (
+								<Button
+									className="nav-btn login-btn mx-md-2 flex-row-reverse"
+									variant="outline-light"
+									as={Link}
+									to="/login"
+								>
+									Login
+								</Button>
+							)}
+							{!user && (
+								<Button
+									className="nav-btn signup-btn my-3 my-md-0 mt-1 mt-md-0 flex-row-reverse"
+									variant="outline-light"
+									as={Link}
+									to="/signup"
+								>
+									Sign Up
+								</Button>
+							)}
+						</Nav>
+					</Navbar.Collapse>
+				)}
 			</Container>
 		</Navbar>
 	);
