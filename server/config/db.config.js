803c0f8381c9e126be0aadbee7dab1819cb78dfe
Karan Patel <s0377009salemstate@gmail.com>
2024-05-18 18:21:30 -0400

    mongoose db connection options added

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const makeDb = (dbUrl) => {
 	try {
-		mongoose.connect(dbUrl);
+		mongoose.connect(dbUrl, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 		console.log("The backend has connected to the MongoDB database.");
 	} catch (error) {
 		console.log(`${error} could not connect to the MongoDB database!`);
