23589f143087807a2521f8106f55444de00b698d
Karan Patel <s0377009salemstate@gmail.com>
2024-05-19 09:43:07 -0400

    userModel schema changed with updated rules

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -6,16 +6,22 @@ const userSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 			label: "username",
+			trim: true,
+			unique: true,
 		},
 		email: {
 			type: String,
 			required: true,
 			label: "email",
+			trim: true,
+			lowercase: true,
+			unique: true,
 		},
 		password: {
 			required: true,
 			type: String,
 			min: 8,
+			trim: true,
 		},
 		date: {
 			type: Date,
