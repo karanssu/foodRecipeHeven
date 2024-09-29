95a6f2d2ea11d97629cd86ab43199ce376121d1e
Karan Patel <s0377009salemstate@gmail.com>
2024-09-28 22:14:36 -0400

    api basic setup

--- server/routes/food/preview/foodPreview.js
+++ server/routes/food/preview/foodPreview.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const foodPreviewModel = require("../../../models/foodPreviewModel");
+require("dotenv").config();
 
 router.get("/preview/", async (req, res) => {
-	const foodPreviews = await foodPreviewModel.find();
-	return res.status(200).json(foodPreviews);
+	const schema = "https://api.spoonacular.com/recipes/complexSearch";
+	const apiKey = process.env.FOOD_API_KEY;
+	const skipCount = 0;
+	const count = 100;
+
+	const url = `${schema}&apiKey=${apiKey}&offset=${skipCount}&number=${count}`;
+
+	const savedFoodPreviews = await foodPreviewModel.find();
+
+	return res.status(200).json(savedFoodPreviews);
 });
 
 module.exports = router;
